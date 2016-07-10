017d9753ca476c3c102113d4338f4117da57fb6c
zhangdongjava <787395919@qq.com>
2016-07-10 16:20:51 +0800

    New changelist

--- src/main/java/test/customTag/Test.java
+++ src/main/java/test/customTag/Test.java
@@ -9,5 +9,8 @@ public class Test {
 	public static void main(String[] args) {
 		BeanFactory beanFactory = new FileSystemXmlApplicationContext("F:\\workspaces\\java2\\customTag\\application.xml");
 		beanFactory.getBean(UserService.class).test();
+		beanFactory.getBean(UserService.class).test2();
+		beanFactory.getBean(UserService.class).test3();
+		beanFactory.getBean(UserService.class).test4();
 	}
 }
--- src/main/java/test/customTag/UserService.java
+++ src/main/java/test/customTag/UserService.java
@@ -3,4 +3,8 @@ package test.customTag;
 public interface UserService {
 
 	public void test();
+	public void test1();
+	public void test2();
+	public void test3();
+	public void test4();
 }
